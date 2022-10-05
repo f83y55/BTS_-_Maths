a6266d2d7aa3aae1ecb8ebfa5580401de39652cd
f83y55 <f83y55@gmail.com>
2022-10-05 09:47:12 +0200

    commit

--- create_index.py
+++ create_index.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 def file_load(filename:str) -> str:
     st = ""
@@ -10,15 +10,28 @@ def file_save(filename:str, st:str):
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(st)
         #print(f"saved as {filename}")
- 
+
+def json_load(filename:str) -> str:
+    data = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+def json_save(filename:str, data:dict):
+    if not data :
+        data = {}
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+
 if __name__ == "__main__" :
     MAINROOT = '.'
     BRIQUE = "brique_index.html"
     dc = {}
     for rep in [element for element in os.listdir(os.path.join(MAINROOT)) if os.path.isdir(os.path.join(MAINROOT, element))] :
         for name in [element for element in os.listdir(os.path.join(MAINROOT, rep)) if element.endswith(".html")] :
-            if os.path.exists(os.path.join(MAINROOT, rep, f"{name[:-5]}.name")) :
-                dc[name] = (file_load(os.path.join(MAINROOT, rep, f"{name[:-5]}.name")), rep)
+            if os.path.exists(os.path.join(MAINROOT, rep, f"{name[:-5]}.json")) :
+                dc[name] = (json_load(os.path.join(MAINROOT, rep, f"{name[:-5]}.json"))["name"], rep)
             else :
                 dc[name] = (name[:-5], rep)
     file_save("index.html", file_load(os.path.join(MAINROOT, BRIQUE)).replace('#####', '\n'.join([f'<li> <a href="{os.path.join(MAINROOT, y[1], x)[:-5]}.html"> {y[0]} </a> </li>' for x, y in dc.items()])))
--- new_doc.py
+++ new_doc.py
@@ -1,14 +1,26 @@
-import unidecode, os
+import unidecode, os, json
 
-def file_load(filename:str) -> str:
+def file_load(filename:str, dc:dict) -> str:
     st = ""
     with open(filename, 'r', encoding='utf-8') as file:
         st = file.read()
-    return st
+    return st.format(**dc)
 
 def file_save(filename:str, st:str):
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(st)
+
+def json_load(filename:str) -> str:
+    data = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+def json_save(filename:str, data:dict):
+    if not data :
+        data = {}
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
 
 def valid_filename(st:str) -> str :
     name = unidecode.unidecode(st)
@@ -25,6 +37,7 @@ BRIQUE = "brique.html"
 if __name__ == "__main__":
     folder_name = input("New folder name :\n")
     web_name = valid_filename(folder_name)
+    data = {"name":folder_name, "web_name":web_name}
     confirmation = input(f"Name : {folder_name}\n Webname : {web_name}\nConfirmation (*:yes/n:no)")
     if confirmation.upper() == "N" :
         pass
@@ -33,8 +46,8 @@ if __name__ == "__main__":
             os.makedirs(os.path.join(MAINROOT, web_name), exist_ok=True)
             os.makedirs(os.path.join(MAINROOT, web_name, "assets"), exist_ok=True)
             if not os.path.exists(os.path.join(MAINROOT, web_name, f"{web_name}.html")) :
-                file_save(os.path.join(MAINROOT, web_name, f"{web_name}.html"), file_load(os.path.join(MAINROOT, BRIQUE)))
-            file_save(os.path.join(MAINROOT, web_name, f"{web_name}.name"), f"{folder_name}")
+                file_save(os.path.join(MAINROOT, web_name, f"{web_name}.html"), file_load(os.path.join(MAINROOT, BRIQUE), data))
+            json_save(os.path.join(MAINROOT, web_name, f"{web_name}.json"), data)
         except Exception as e:
             print(e)
 
