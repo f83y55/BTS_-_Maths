486a0ab06ee4f88d22ae0d1a43bc31b10eff6872
f83y55 <f83y55@gmail.com>
2023-01-19 01:14:12 +0100

    commit

--- create_index.py
+++ create_index.py
@@ -27,6 +27,7 @@ def json_save(filename:str, data:dict):
 if __name__ == "__main__" :
     MAINROOT:str = '.'
     BRIQUE:str = "brique_index.html"
+    NAV:str = os.path.join("nav", "nav.html")
     dc:dict = {}
     ls:list = [element for element in os.listdir(os.path.join(MAINROOT)) if os.path.isdir(os.path.join(MAINROOT, element))]
     ls.sort()
@@ -34,7 +35,12 @@ if __name__ == "__main__" :
         for name in [element for element in os.listdir(os.path.join(MAINROOT, rep)) if element.endswith(".html")] :
             if os.path.exists(os.path.join(MAINROOT, rep, f"{name[:-5]}.json")) :
                 dc[name] = (json_load(os.path.join(MAINROOT, rep, f"{name[:-5]}.json"))["name"], rep)
-            file_save("index.html", file_load(os.path.join(MAINROOT, BRIQUE)).replace('#####', '\n'.join([f'<li> <a href="{os.path.join(MAINROOT, y[1], x)[:-5]}.html"> {y[0]} </a> </li>' for x, y in dc.items()])))
+            html = file_load(os.path.join(MAINROOT, BRIQUE))
+            html.replace('#####list#####', '\n'.join([f'<li> <a href="{os.path.join(MAINROOT, y[1], x)[:-5]}.html"> {y[0]} </a> </li>' for x, y in dc.items()]))
+            nav = file_load(os.path.join(MAINROOT, NAV))
+            nav.replace("@import \"../style/nav.css", "@import \"./style/nav.css")
+            html.replace('#####nav#####', nav)
+            file_save("index.html", html)
             
 
 
